d0ae01ce20db104b4c2f4ec6993b3c352708cf07
kalmaske <kalmaske@uncc.edu>
2017-08-30 12:28:27 -0400

    fixes

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,6 +13,26 @@ var friends = [
         "name": "Tweety",
         "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/0/02/Tweety.svg/570px-Tweety.svg.png",
         "answers": [4, 1, 3, 5, 5, 1, 3, 5, 1, 5]
+    },
+    {
+        "name": "Pepé Le Pew",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Pep%C3%A9_Le_Pew.svg/928px-Pep%C3%A9_Le_Pew.svg.png",
+        "answers": [3,5,1,4,3,1,3,4,4,3]
+    },
+    {
+        "name": "Tasmanian Devil",
+        "photo": "http://hddfhm.com/images/looney-tunes-tasmanian-devil-clipart-8.jpg",
+        "answers": [2,5,2,1,1,1,4,3,2,5]
+    },
+    {
+        "name": "Yosemite Sam",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Yosemite_Sam.svg/640px-Yosemite_Sam.svg.png",
+        "answers": [1,4,3,3,2,1,2,4,3,5]
+    },
+    {
+        "name": "Miss Prissy",
+        "photo": "https://s-media-cache-ak0.pinimg.com/originals/13/36/ee/1336ee1a15755e8cdb4e0ea938b62a11.jpg",
+        "answers": [3,3,5,5,4,4,3,3,5,5]
     }
 ];
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,18 +1,17 @@
 var path = require('path');
 
-module.exports = function(app) {
-	app.get('/survey', function(req, res){
+module.exports = function (app) {
+	app.get('/survey', function (req, res) {
 		res.sendFile(path.join(__dirname, '../public/survey.html'));
-    });
-    
-    app.get('/style.css',function(req, res){
+	});
+
+	app.get('/style.css', function (req, res) {
 		res.sendFile(path.join(__dirname + '/../public/style.css'));
 	});
 
-	app.use(function(req, res){
+	app.use(function (req, res) {
 		res.sendFile(path.join(__dirname + '/../public/home.html'));
-    });
-    
-    
-}
+	});
 
+
+}
